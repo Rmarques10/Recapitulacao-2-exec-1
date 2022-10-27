5ef0cebf7cec40e01797e8b1925bd131b5710383
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 11:50:50 -0300

    Recapitulação

---
+++ Recapitulacao 2 exec 1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não */
+
+
+int numero;
+Console.WriteLine("Informe um número");
+numero = int.Parse(Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.WriteLine("Negativo");
+}
+else
+{
+    Console.WriteLine("Não Negativo");
+}
